b662d6aae843c8bb790199262d86033e6f4d8d27
ffrrzz820 <144077422+ffrrzz820@users.noreply.github.com>
2023-11-20 16:07:30 +0900

    for

--- tab.js
+++ tab.js
@@ -5,22 +5,61 @@
 // 좋은 관습 :
 // 자주쓰는 셀렉터 변수에 넣어쓰기
 
-const 버튼1 = document.querySelectorAll('.tab-button')[0];
-const 버튼2 = document.querySelectorAll('.tab-button')[1];
-const 버튼3 = document.querySelectorAll('.tab-button')[2];
+// for 반복문 쓰면
+// 코드 복붙해줍니다
 
-버튼1.addEventListener('click', function(){
-    버튼1.classList.add('orange');
-    버튼2.classList.remove('orange');
-    버튼3.classList.remove('orange');
-})
-버튼2.addEventListener('click', function(){
-    버튼2.classList.add('orange');
-    버튼1.classList.remove('orange');
-    버튼3.classList.remove('orange');
-})
-버튼3.addEventListener('click', function(){
-    버튼3.classList.add('orange');
-    버튼1.classList.remove('orange');
-    버튼2.classList.remove('orange');
-})
+// for 반복문 쓰는 법
+// for (횟수){
+//     복붙할코드
+// }
+const 버튼 = document.querySelectorAll('.tab-button');
+const 설명 = document.querySelectorAll('.tab-content');
+const 갯수 = 버튼.length;
+
+for (let i = 0; i < 갯수; i++){ // 3회라는 뜻임
+
+    버튼[i].addEventListener('click', function(){
+        버튼.forEach((e) => {
+            e.classList.remove('orange');
+        });
+        설명.forEach((e) => {
+            e.classList.remove('show');
+        });
+        
+        버튼[i].classList.add('orange');
+        설명[i].classList.add('show');
+    })
+}
+// 시작은 i = 0
+// 복붙할 때마다 i+1
+// 3 되면 끝내셈
+
+// 변수i    복붙여부
+// 0        ok
+// 1        ok
+// 2        ok
+// 3        X
+
+// for 문법은 코드복붙보다는 반복실행이 맞음(별차이없음)
+
+
+
+// const 버튼1 = document.querySelectorAll('.tab-button')[0];
+// const 버튼2 = document.querySelectorAll('.tab-button')[1];
+// const 버튼3 = document.querySelectorAll('.tab-button')[2];
+
+// 버튼1.addEventListener('click', function(){
+//     버튼1.classList.add('orange');
+//     버튼2.classList.remove('orange');
+//     버튼3.classList.remove('orange');
+// })
+// 버튼2.addEventListener('click', function(){
+//     버튼2.classList.add('orange');
+//     버튼1.classList.remove('orange');
+//     버튼3.classList.remove('orange');
+// })
+// 버튼3.addEventListener('click', function(){
+//     버튼3.classList.add('orange');
+//     버튼1.classList.remove('orange');
+//     버튼2.classList.remove('orange');
+// })
